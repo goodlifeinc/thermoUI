6e0a143892d20c26f5323086a79e761b01d16974
goddlifeinc <genata93@gmail.com>
2019-01-26 18:42:29 +0200

    update build

--- ui/preact.config.js
+++ ui/preact.config.js
@@ -1,5 +1,21 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 export default (config, env, helpers) => {
 	let rules = helpers.getRules(config);
+
+	// https://github.com/developit/preact-cli/issues/593
+	// remove the old
+	const uglifyJsPlugin = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+	if (uglifyJsPlugin) {
+		const { index } = uglifyJsPlugin;
+		// console.info(`helpers.getPluginsByName(config, 'UglifyJsPlugin'): ${JSON.stringify(helpers.getPluginsByName(config, 'UglifyJsPlugin'))}`);
+		// console.info(`index: ${index}`);
+		// console.info(`before config.plugins: ${JSON.stringify(config.plugins[index])}`);
+		// console.info(`before config.plugins.length: ${JSON.stringify(config.plugins.length)}`);
+		config.plugins.splice(index, 1, new UglifyJsPlugin());
+		// console.info(`after config.plugins: ${JSON.stringify(config.plugins[index])}`);
+		// console.info(`after config.plugins.length: ${JSON.stringify(config.plugins.length)}`);
+	}
     
 	let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
 	rule.exclude = /(node_modules)/;
@@ -7,7 +23,7 @@ export default (config, env, helpers) => {
 	const rulePlotly = {
 		index: Object.keys(rules).length,
 		rule: {
-			test: /\.js$/,
+			test: /\.js?$/,
 			use: [
 				'ify-loader',
 				'transform-loader?plotly.js/tasks/compress_attributes.js'
